ba28e5c87cfc0f25a79926847c24974e0f0b2b44
Muhammad-Daniyal-Jawad1 <130592856+Muhammad-Daniyal-Jawad1@users.noreply.github.com>
2025-01-16 11:52:35 +0500

    Continuing TopDoctors.

--- front-end/src/components/TopDoctors.jsx
+++ front-end/src/components/TopDoctors.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { doctors } from '../assets/assets'
 
 const TopDoctors = () => {
   return (
@@ -6,7 +7,11 @@ const TopDoctors = () => {
         <h1>Top Doctors to Book</h1>
         <p>Simply browse through our extensive list of trusted doctors.</p>
         <div>
-          
+          {doctors.slice(0,10).map((item,index)=>(
+            <div>
+              
+            </div>
+          ))}
         </div>
     </div>
   )
